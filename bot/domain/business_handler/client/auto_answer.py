d7f4d679e5504c2747e5d904fe51f90da31c711a
artem <artemqq5@gmail.com>
2025-04-22 11:52:30 +0300

    Update auto_answer.py

--- bot/domain/business_handler/client/auto_answer.py
+++ bot/domain/business_handler/client/auto_answer.py
@@ -22,7 +22,7 @@ deepseek = DeepSeekAPI()
 router.business_message.middleware(ChatMessageMiddleware())
 router.business_message.middleware(ClientBusinessMiddleware())
 
-model = whisper.load_model("medium")
+model = whisper.load_model("base")
 
 #  базовая «реакция» — задержка, если предыдущее сообщение было давно
 MIN_DELAY, MAX_DELAY = 5, 30  # сек
